da6bf7689ce4e8edd94b619ac3aea44edd41ef41
FedeHide <larcdian@gmail.com>
2024-05-23 21:15:46 -0300

    add: searchBar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,14 @@
 import Link from 'next/link'
 import Menu from './Menu'
 import Image from 'next/image'
+import SearchBar from './SearchBar'
+import NavIcons from './NavIcons'
 
 export default function Navbar() {
 	return (
 		<div className="h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative">
 			{/* MOBILE */}
-			<div className="h-full flex items-center justify-between">
+			<div className="h-full flex items-center justify-between md:hidden">
 				<Link href="/">
 					<div className="text-2xl tracking-wide">PURRIS</div>
 				</Link>
@@ -15,12 +17,15 @@ export default function Navbar() {
 			{/* DESKTOP */}
 			<div className="hidden md:flex items-center justify-between gap-8 h-full">
 				<div className="w-1/3">
-					<Link href="/">
+					<Link className="flex items-center gap-3" href="/">
 						<Image src="/assets/logo.png" alt="logo" width={24} height={24} />
 						<div className="text-2xl tracking-wide">PURRIS</div>
 					</Link>
 				</div>
-				<div className="w-2/3"></div>
+				<div className="w-2/3 flex items-center justify-between gap-8">
+					<SearchBar />
+					<NavIcons />
+				</div>
 			</div>
 		</div>
 	)
