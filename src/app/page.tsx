cb6b1cef98207dbad5aa96fe06851a5fa3785c59
FedeHide <larcdian@gmail.com>
2024-05-25 03:11:37 -0300

    add: hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import Slider from '@/components/Slider'
+
 export default function Home(): JSX.Element {
-    return (
-        <>
-            <h1>Home</h1>
-            <p className='text-3xl'>Project</p>
-        </>
-    )
+	return (
+		<>
+			<div>
+				<Slider />
+			</div>
+		</>
+	)
 }
