2dd7cd93d252a5c5b35d27b3909a1420e35d8952
FedeHide <larcdian@gmail.com>
2024-05-31 20:00:43 -0300

    add: single page product slug right section

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -8,7 +8,46 @@ export default function SinglePage(): JSX.Element {
 				<ProductImages />
 			</div>
 			{/* ProductDetails */}
-			<div className="w-full lg:w-1/2 flex flex-col gap-6"></div>
+			<div className="w-full lg:w-1/2 flex flex-col gap-6">
+				<h1 className="text-4xl font-medium">Product Name</h1>
+				<p className="text-gray-500">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil eligendi delectus
+					ad quam eveniet incidunt sunt consequuntur porro optio saepe?
+				</p>
+				<div className="h-[2px] bg-gray-100"></div>
+				<div className="flex items-center gap-4">
+					<h3 className="text-xl text-gray-500 line-through">$59</h3>
+					<h2 className="font-medium text-2xl">$49</h2>
+				</div>
+				<div className="h-[2px] bg-gray-100"></div>
+				{/* CustomizeProducts */}
+				{/* Add */}
+				<div className="h-[2px] bg-gray-100"></div>
+				<div className="text-sm">
+					<h4 className="font-medium mb-4">Title</h4>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus
+						fugit, repudiandae ab quae perferendis quos beatae est voluptatem delectus
+						velit? Eius natus nulla cupiditate dolore.
+					</p>
+				</div>
+				<div className="text-sm">
+					<h4 className="font-medium mb-4">Title</h4>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus
+						fugit, repudiandae ab quae perferendis quos beatae est voluptatem delectus
+						velit? Eius natus nulla cupiditate dolore.
+					</p>
+				</div>
+				<div className="text-sm">
+					<h4 className="font-medium mb-4">Title</h4>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus
+						fugit, repudiandae ab quae perferendis quos beatae est voluptatem delectus
+						velit? Eius natus nulla cupiditate dolore.
+					</p>
+				</div>
+			</div>
 		</div>
 	)
 }
