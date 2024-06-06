5eccf45104d9e6513778af6cc221008d89bf4bac
FedeHide <larcdian@gmail.com>
2024-06-05 23:33:27 -0300

    feat: fetching categories from DB and add skeleton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 import CategoryList from '@/components/categoryList/CategoryList'
 import ProductList from '@/components/ProductList'
 import Slider from '@/components/heroSlider/Slider'
-import { wixClientServer } from '@/lib/wixClientServer'
+// import { wixClientServer } from '@/lib/wixClientServer'
+import { Suspense } from 'react'
+import Skeleton from '@/components/Skeleton'
 // import { useEffect } from 'react'
 // import { useWixClient } from '@/hooks/useWixClient'
 
@@ -21,9 +23,9 @@ export default async function Home(): Promise<JSX.Element> {
 	// 	getProducts()
 	// }, [wixClient])
 
-	const wixClient = await wixClientServer()
-	const res = await wixClient.products.queryProducts().find()
-	console.log(res)
+	// const wixClient = await wixClientServer()
+	// const res = await wixClient.products.queryProducts().find()
+	// console.log(res)
 
 	return (
 		<>
@@ -31,7 +33,12 @@ export default async function Home(): Promise<JSX.Element> {
 				<Slider />
 				<div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
 					<h1 className="text-2xl">Featured Products</h1>
-					<ProductList />
+					<Suspense fallback={<Skeleton />}>
+						<ProductList
+							categoryId={process.env.FEATURED_PRODUCTS_CATEGORY_ID ?? ''}
+							limit={4}
+						/>
+					</Suspense>
 				</div>
 				<div className="mt-24">
 					<h1 className="text-2xl px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 mb-12">
@@ -41,7 +48,10 @@ export default async function Home(): Promise<JSX.Element> {
 				</div>
 				<div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
 					<h1 className="text-2xl">New Products</h1>
-					<ProductList />
+					<ProductList
+						categoryId={process.env.NEW_PRODUCTS_CATEGORY_ID ?? ''}
+						limit={4}
+					/>
 				</div>
 			</div>
 		</>
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,71 +1,75 @@
+import { wixClientServer } from '@/lib/wixClientServer'
+import type { products } from '@wix/stores'
 import Image from 'next/image'
 import Link from 'next/link'
+import DOMPurify from 'isomorphic-dompurify'
 
-const products = [
-	{
-		id: 1,
-		title: 'Bonsai Tree',
-		description: 'Elegant indoor potted plant',
-		price: 45,
-		img: 'https://images.pexels.com/photos/4050790/pexels-photo-4050790.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-		img2: 'https://images.pexels.com/photos/8408547/pexels-photo-8408547.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-	},
-	{
-		id: 2,
-		title: 'Red Cocktail Dress',
-		description: 'Stylish evening wear',
-		price: 125,
-		img: 'https://images.pexels.com/photos/16922808/pexels-photo-16922808/free-photo-of-mujer-modelo-maqueta-sombrero.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-		img2: 'https://images.pexels.com/photos/16922810/pexels-photo-16922810/free-photo-of-mujer-sentado-sombrero-gorro.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-	},
-	{
-		id: 3,
-		title: 'Checkered Shirt',
-		description: 'Casual checkered shirt',
-		price: 30,
-		img: 'https://images.pexels.com/photos/18185494/pexels-photo-18185494/free-photo-of-hombre-calle-chaqueta-joven.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-		img2: 'https://images.pexels.com/photos/18185493/pexels-photo-18185493/free-photo-of-hombre-chaqueta-joven-guapo.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-	},
-	{
-		id: 4,
-		title: 'Classic Hoodie',
-		description: 'Comfortable casual wear',
-		price: 40,
-		img: 'https://images.pexels.com/photos/2932727/pexels-photo-2932727.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-		img2: 'https://images.pexels.com/photos/2932740/pexels-photo-2932740.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-	},
-]
+const PRODUCT_PER_PAGE = 20
 
-export default function ProductList(): JSX.Element {
+export default async function ProductList({
+	categoryId,
+	limit,
+}: {
+	categoryId: string
+	limit?: number
+}): Promise<JSX.Element> {
+	const wixClient = await wixClientServer()
+	const res = await wixClient.products
+		.queryProducts()
+		.eq('collectionIds', categoryId)
+		.limit(limit ?? PRODUCT_PER_PAGE)
+		.find()
+
 	return (
 		<div className="flex gap-x-8 gap-y-16 justify-between flex-wrap mt-12">
-			{products.map((product) => (
+			{res.items.map((product: products.Product) => (
 				<Link
-					key={product.id}
+					key={product._id}
 					className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
-					href="/test"
+					href={`/${product.slug}`}
 				>
 					<div className="relative w-full h-80">
 						<Image
-							src={product.img}
-							alt={product.title}
+							src={product.media?.mainMedia?.image?.url ?? '/product.png'}
+							alt={(product.name !== null).toString()}
 							className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity ease-in-out duration-500"
 							sizes="25vw"
 							fill
 						/>
-						<Image
-							src={product.img2}
-							alt={product.title}
-							className="absolute object-cover rounded-md"
-							sizes="25vw"
-							fill
-						/>
+						{product.media?.items !== undefined && product.media.items.length > 1 && (
+							<Image
+								src={product.media?.items[1]?.image?.url ?? '/product.png'}
+								alt={(product.name !== null).toString()}
+								className="absolute object-cover rounded-md"
+								sizes="25vw"
+								fill
+							/>
+						)}
 					</div>
-					<div className="flex justify-between">
-						<span className="font-medium">{product.title}</span>
-						<span className="font-semibold">${product.price}</span>
+					<div className="flex justify-between sm:gap-4">
+						<span className="font-medium">{product.name}</span>
+						<div className="flex flex-row items-center gap-4 md:flex-row md:gap-4 lg:flex-col xl:flex-row ">
+							{product.price?.discountedPrice !== product.price?.price && (
+								<span className="font-semibold text-gray-500 line-through">
+									${product.price?.discountedPrice}
+								</span>
+							)}
+							<span className="font-semibold">${product.price?.price}</span>
+						</div>
 					</div>
-					<div className="text-sm text-gray-500">{product.description}</div>
+					{product.additionalInfoSections !== undefined &&
+						product.additionalInfoSections !== null && (
+							<div
+								className="text-sm text-gray-500"
+								dangerouslySetInnerHTML={{
+									__html: DOMPurify.sanitize(
+										product.additionalInfoSections.find(
+											(section: any) => section.title === 'shortDesc',
+										)?.description ?? 'No description available',
+									),
+								}}
+							></div>
+						)}
 					<button className="w-max rounded-2xl ring-1 ring-clrPrimary text-clrPrimary py-2 px-4 text-xs hover:bg-clrPrimary md:active:bg-clrPrimary hover:text-white md:active:text-white transition-colors ease-in duration-200">
 						Add to Cart
 					</button>
